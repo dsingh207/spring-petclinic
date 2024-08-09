41027cea1303558b6ebf0ae7b5cae53fe1591cb6
dsingh207 <deepaksinghkhati111@gmail.com>
2024-08-09 17:42:41 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,10 +4,10 @@
 echo "Stopping Apache server"
  
 # Stop the Apache service
-sudo systemctl stop httpd
+sudo systemctl stop apache2
  
 # Check if the Apache service stopped successfully
-if ! systemctl is-active --quiet httpd; then
+if ! systemctl is-active --quiet apache2; then
     echo "Apache has stopped successfully"
     exit 0
 else
