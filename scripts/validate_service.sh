c9827894b3b1184704d3b977d8ed18bb66957cd6
dsingh207 <deepaksinghkhati111@gmail.com>
2024-08-09 17:39:25 +0530

    Create validate_service.sh

---
+++ scripts/validate_service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo “Validating Apache service”
+
+if systemctl is-active –quiet httpd; then
+
+    echo “Apache is running”
+
+    exit 0
+
+else
+
+    echo “Apache is not running”
+
+    exit 1
+
+fi
