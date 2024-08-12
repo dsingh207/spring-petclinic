3a715c1a284086149334e103535c16d3a1617bb5
dsingh207 <deepaksinghkhati111@gmail.com>
2024-08-12 21:36:30 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
- 
-# Echo a message for logging
-echo "Starting Apache server"
- 
-# Start the Apache service
-sudo systemctl start apache2
- 
-# Check if the Apache service started successfully
-if systemctl is-active --quiet apache2; then
-    echo "Apache is running"
-    exit 0
-else
-    echo "Apache failed to start"
-    exit 1
-fi
+
+nohup java -jar /opt/*.jar > /dev/null 2>&1 &
